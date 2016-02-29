01b0efe2928cbe7ed387be0bae99c8fe85f033e9
khunchye <sirichai_v@uih.co.th>
2016-02-29 12:02:00 +0700

    test finish

---
+++ app/src/main/java/th/co/sirichai_vuih/kcrestaurant/MyManage.java
@@ -0,0 +1,60 @@
+package th.co.sirichai_vuih.kcrestaurant;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by masterUNG on 2/29/16 AD.
+ */
+public class MyManage {
+
+    //Explicit
+    private MyOpenHelper myOpenHelper;
+    private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
+
+    public static final String user_table = "userTABLE";
+    public static final String column_id = "_id";
+    public static final String column_user = "User";
+    public static final String column_pass = "Password";
+    public static final String column_name = "Name";
+
+    public static final String food_table = "foodTABLE";
+    public static final String column_food = "Food";
+    public static final String column_source = "Source";
+    public static final String column_price = "Price";
+
+
+    public MyManage(Context context) {
+
+        //Create Database
+        myOpenHelper = new MyOpenHelper(context);
+        writeSqLiteDatabase = myOpenHelper.getWritableDatabase();
+        readSqLiteDatabase = myOpenHelper.getReadableDatabase();
+
+    }   // Constructor
+
+    public long addFood(String strFood,
+                        String strPrice,
+                        String strSource) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(column_food, strFood);
+        contentValues.put(column_price, strPrice);
+        contentValues.put(column_source, strSource);
+
+        return writeSqLiteDatabase.insert(food_table, null, contentValues);
+    }
+
+    //Add Value to userTABLE
+    public long addUser(String string,
+                        String strPassword,
+                        String strName){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(column_user, strUser);
+        contentValues.put(column_pass, strPassword);
+        contentValues.put(column_name, strName);
+        return 0;
+    }
+
+
+}   // Main Class
